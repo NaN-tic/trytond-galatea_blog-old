7e89ec2e9da3fc8d07170684f2c1a043e2bf6e25
resteve <resteve@zikzakmedia.com>
2014-10-13 11:26:00 +0200

    Header files

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-#~ #This file is part galatea_esale module for Tryton.
+#~ #This file is part galatea_blog module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 import logging
